cfc5e75eaa0339674a305b68e64c7ba4ce1e24d1
JuanPabloRobledo <juanpablorobledo1@hotmail.com>
2023-01-03 20:55:52 -0300

    h

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.sr = ScrollReveal();
 
     sr.reveal('.navbar', {
-        duration: 1000,
+        duration: 500,
         origin: 'bottom' ,
         distance: '-100px'
     });
@@ -32,12 +32,18 @@ window.sr = ScrollReveal();
 
     sr.reveal('.flex-container', {
         duration: 1999,
-        origin: 'bottom' ,
+        origin: 'right' ,
         distance: '-100px'
     });
 
     sr.reveal('.section4-1', {
         duration: 1999,
         origin: 'bottom' ,
+        distance: '-100px'
+    });
+
+    sr.reveal('.textos-text1', {
+        duration: 1000,
+        origin: 'top' ,
         distance: '-100px'
     });
