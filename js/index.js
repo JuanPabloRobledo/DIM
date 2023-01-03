3d1c680e1cae6b15997da739ea97f9a7e8e8092f
JuanPabloRobledo <juanpablorobledo1@hotmail.com>
2023-01-03 12:21:58 -0300

    H

---
+++ js/index.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 1000,
+        origin: 'bottom' ,
+        distance: '-100px'
+    });
+
+    sr.reveal('.flexbox', {
+        duration: 1999,
+        origin: 'bottom' ,
+        distance: '-100px'
+    });
+
+    sr.reveal('.banner2', {
+        duration: 1999,
+        origin: 'bottom' ,
+        distance: '-100px'
+    });
+
+    sr.reveal('.flex-container', {
+        duration: 1999,
+        origin: 'bottom' ,
+        distance: '-100px'
+    });
