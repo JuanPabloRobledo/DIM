f2cf8f5c49f107bd25aa85b32d2e07a33c57eff6
JuanPabloRobledo <juanpablorobledo1@hotmail.com>
2023-02-28 10:28:30 -0300

    h

--- js/indexNav.js
+++
@@ -1,6 +0,0 @@
-const menuHam = document.querySelector(".menu-ham");
-const lista = document.querySelector(".lista"); 
-
-menuHam.addEventListener("click", () => {
-    lista.classList.toggle("lista_visible");
-});
---
+++ js/java2.js
@@ -0,0 +1,45 @@
+window.addEventListener("scroll", function () {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("abajo", window.scrollY > 0);
+})
+const navBar = document.querySelector("nav"),
+    menuBtns = document.querySelectorAll(".menu-icon"),
+    overlay = document.querySelector(".overlay");
+
+menuBtns.forEach((menuBtn) => {
+    menuBtn.addEventListener("click", () => {
+        navBar.classList.toggle("open");
+    });
+});
+
+overlay.addEventListener("click", () => {
+    navBar.classList.remove("open");
+});
+
+window.sr = ScrollReveal();
+
+sr.reveal('.banner-h1', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-500px'
+});
+sr.reveal('.flexbox-img', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-200px'
+});
+sr.reveal('.flexbox-banner', {
+    duration: 1000,
+    origin: 'right',
+    distance: '-200px'
+});
+sr.reveal('.card', {
+    duration: 1500,
+    origin: 'left',
+    distance: '-200px'
+});
+sr.reveal('.banner-h1-3', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-200px'
+});
